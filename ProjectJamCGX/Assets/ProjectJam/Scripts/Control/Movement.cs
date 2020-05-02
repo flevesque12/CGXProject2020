27cfd299bb7697187469e85e5d72d2027cd6ff30
Frederic Levesque <flevesque124@gmail.com>
2020-05-01 23:01:09 -0400

    Character skeleton code is added

---
+++ ProjectJamCGX/Assets/ProjectJam/Scripts/Control/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ this is where we going to use physics(or charactercontroller) and animation update with animator
+     */
+
+public class Movement : MonoBehaviour
+{
+    Animator m_Animator;
+    Rigidbody m_Rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
