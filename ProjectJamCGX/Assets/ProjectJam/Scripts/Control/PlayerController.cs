c8aa063963d2fbbd1b895d58b4c24b178d5ad7e0
chev_dev <christian.chevrette@hotmail.com>
2020-05-03 07:46:24 -0400

    nav mesh, camera smoothing, audio, testbuild
    title

--- ProjectJamCGX/Assets/ProjectJam/Scripts/Control/PlayerController.cs
+++ ProjectJamCGX/Assets/ProjectJam/Scripts/Control/PlayerController.cs
@@ -1,24 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 /*
  this is where we interact with input
      */
 public class PlayerController : MonoBehaviour
 {
     public float m_InteractionRange = 1.0f;
-
+    private NavMeshAgent agent;
+    public bool isRunning = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+       agent  = GetComponent<NavMeshAgent>();
+
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        if(agent.remainingDistance <= agent.stoppingDistance)
+        {
+            isRunning = false;
+        }
+        else
+        {
+            isRunning = true;
+        }
+
+
     }
+
+   
 
 
     private void Interaction()
---
+++ ProjectJamCGX/Assets/ProjectJam/Scripts/Director.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour
+{
+    public AudioSource audio;
+    float timeInterval = 0f;
+    private void Update()
+    {
+        if (audio.volume < .155 && Time.fixedTime + 1f >= timeInterval)
+        {
+            timeInterval = Time.fixedTime;
+            audio.volume += .001f;
+        }
+    }
+}
