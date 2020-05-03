645bb5b0db5ce2d96fef3b9f2823fcf012966891
chev_dev <christian.chevrette@hotmail.com>
2020-05-03 12:21:09 -0400

    hexes, fog

--- ProjectJamCGX/Assets/ProjectJam/Scripts/Fog.cs
+++ ProjectJamCGX/Assets/ProjectJam/Scripts/Fog.cs
@@ -6,6 +6,10 @@ public class Fog : MonoBehaviour
 {
     public Vector3 pos;
     public Transform t;
+
+    float scrollSpeed = 0.5f;
+    public Renderer rend;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +20,7 @@ public class Fog : MonoBehaviour
     void Update()
     {
         gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z) + t.position;
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
     }
 }
