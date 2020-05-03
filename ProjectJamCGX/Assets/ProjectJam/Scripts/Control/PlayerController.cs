598f67583976c520c73d249b6c7be5e467893bb4
Frederic Levesque <flevesque124@gmail.com>
2020-05-03 14:22:22 -0400

    Character run with animations added and camera fixed, some change to the prefabs added

--- ProjectJamCGX/Assets/ProjectJam/Scripts/Control/PlayerController.cs
+++ ProjectJamCGX/Assets/ProjectJam/Scripts/Control/PlayerController.cs
@@ -1,38 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 /*
  this is where we interact with input
      */
 
-
-public class Pcchev : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float m_InteractionRange = 1.0f;
     public bool isRunning = false;
-    CameraRayCaster m_CameraRayCaster;
-    NavMeshAgent m_Agent;
+    private CameraRayCaster m_CameraRayCaster;
+    private NavMeshAgent m_Agent;
+    private Animator m_Animator;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         m_CameraRayCaster = Camera.main.GetComponent<CameraRayCaster>();
         m_Agent = GetComponent<NavMeshAgent>();
+        m_Animator = GetComponent<Animator>();
     }
 
-
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetMouseButton(0)) {
-           
-                m_Agent.destination = m_CameraRayCaster.Hit.point;
-                Debug.Log("Destination");
-            
+        if (Input.GetMouseButton(0))
+        {
+            m_Agent.destination = m_CameraRayCaster.Hit.point;
         }
-        
-        if(m_Agent.remainingDistance <= m_Agent.stoppingDistance)
+
+        if (m_Agent.remainingDistance <= m_Agent.stoppingDistance)
         {
             isRunning = false;
         }
@@ -40,15 +37,21 @@ public class Pcchev : MonoBehaviour
         {
             isRunning = true;
         }
+
+        UpdateAnimator();
     }
-        
-    
 
-   
+    private void UpdateAnimator()
+    {
+        //get the velocity from the navmesh agent
+        Vector3 _velocity = m_Agent.velocity;
 
+        Vector3 localVelocity = transform.InverseTransformDirection(_velocity);
+        float _speed = localVelocity.z;
+        m_Animator.SetFloat("speed", _speed);
+    }
 
     private void Interaction()
     {
-
     }
 }
